5af736a532f09903cae79125d80f6f6c44f62949
Pablo Nicolas Guala <112034712+NikoSempai27@users.noreply.github.com>
2023-11-06 22:33:36 -0300

    Clase 1 de JavaScript del cuarto semestre
    CSLeccion 01 comenzamos con la creación de un e-comerce básico

---
+++ E-Commerce01/Client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/Client/media/pngwing.com (17).png",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/Client/media/pngwing.com (21).png",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quanty: 1,
+        img: "/Client/media/pngwing.com (20).png",
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 510,
+        quanty: 1,
+        img: "/Client/media/pngwing.com (18).png",
+    },
+];
